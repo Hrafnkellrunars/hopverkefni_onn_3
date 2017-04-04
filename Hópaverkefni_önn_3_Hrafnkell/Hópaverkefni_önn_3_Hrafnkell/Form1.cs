905baca74ca00e34361f5b9959e9e178dd486ce9
Hrafnkellrunars <hrafnkellrunars@gmail.com>
2017-04-04 15:21:55 +0000

    Forritið
    bara til að sjá ef forritið tengist við gagnagrunninn

---
+++ Hópaverkefni_önn_3_Hrafnkell/Hópaverkefni_önn_3_Hrafnkell/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Hrafnkell Rúnarsson & Kristinn Þór Kristinsson
+   04.04.2017
+   Hópverkefni á 3 önn */
+
+namespace Hópaverkefni_önn_3_Hrafnkell
+{
+    public partial class Form1 : Form
+    {
+        Gagnagrunnur gagnagrunnur = new Gagnagrunnur(); // tenging við gagnagrunns classið
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                gagnagrunnur.TengingVidGagnagrunn(); // tengist við gagnagrunninn
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        public void Hledsla() // void til að endurhlaða gagnagrunninn sjálfkrafa
+        {
+            listView1.Clear(); // hreinsar listviewið
+
+            listView1.Columns.Add("ID", 70); // sýnir nafn stökkvarans og plássið sem sá reitur fær
+            listView1.Columns.Add("Nafn", 70); // sýnir stokk stökkvarans og plássið sem sá reitur fær
+            listView1.Columns.Add("Sort", 70); // sýnir stokk stökkvarans og plássið sem sá reitur fær
+            listView1.Columns.Add("Þyngd", 70); // sýnir stokk stökkvarans og plássið sem sá reitur fær
+            listView1.Columns.Add("Mjólk", 70); // sýnir nafn stökkvarans og plássið sem sá reitur fær
+            listView1.Columns.Add("ull", 70); // sýnir stokk stökkvarans og plássið sem sá reitur fær
+            listView1.Columns.Add("afkvæmi", 70); // sýnir stokk stökkvarans og plássið sem sá reitur fær
+            listView1.Columns.Add("einkunlæris", 70); // sýnir stokk stökkvarans og plássið sem sá reitur fær
+            listView1.Columns.Add("frjósemi", 70); // sýnir nafn stökkvarans og plássið sem sá reitur fær
+            listView1.Columns.Add("gerð", 70); // sýnir stokk stökkvarans og plássið sem sá reitur fær
+            listView1.Columns.Add("einkunmalir", 70); // sýnir stokk stökkvarans og plássið sem sá reitur fær
+
+            List<string> linur = new List<string>();
+
+            string[] arr = new string[11];
+
+            ListViewItem itm;
+
+            try
+            {
+                linur = gagnagrunnur.LesautSQLToflu();
+
+                foreach (string lin in linur)
+                {
+                    string[] linaUrLista = lin.Split(':');
+                    string id = linaUrLista[0];
+                    string nafn = linaUrLista[1];
+                    string stokk = linaUrLista[2];
+                    string thyngd = linaUrLista[3];
+                    string mjolk = linaUrLista[4];
+                    string ull = linaUrLista[5];
+                    string afkvaemi = linaUrLista[6];
+                    string einkunlaeris = linaUrLista[7];
+                    string frjosemi = linaUrLista[8];
+                    string gerd = linaUrLista[9];
+                    string einkunmalir = linaUrLista[10];
+
+                    arr[0] = id; // array fyrir nafn
+                    arr[1] = nafn; // array fyrir stök
+                    arr[2] = stokk; //
+                    arr[3] = thyngd; //
+                    arr[4] = mjolk; //
+                    arr[5] = ull; //
+                    arr[6] = afkvaemi; //
+                    arr[7] = einkunlaeris; //
+                    arr[8] = frjosemi; //
+                    arr[9] = gerd; //
+                    arr[10] = einkunmalir; //
+                    
+
+
+                    itm = new ListViewItem(arr);
+                    listView1.Items.Add(itm);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hledsla(); // endurhleður sig
+        }
+    }
+}
---
+++ Hópaverkefni_önn_3_Hrafnkell/Hópaverkefni_önn_3_Hrafnkell/Gagnagrunnur.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Hópaverkefni_önn_3_Hrafnkell
+{
+    class Gagnagrunnur
+    {
+        private string server; // segir til um hvar serverinn er hýstur, hjá okkur er það localhost
+        private string database; // nafnið á gagnagrunninum sem verið er að nota
+        private string uid; // MySQL Notendanafnið
+        private string password; // MySQLlykilorðið
+
+        string tengistrengur = null; // tengir saman strenginn og databeisið
+        string fyrirspurn = null; // inniheldur fyrirspurn
+
+        MySqlConnection sqltenging; // opnar tengingu við gagnagrunn
+        MySqlCommand nySQLskipun; // framkvæmir fyrirpurninar
+        MySqlDataReader sqllesari = null; // les úr SQL gagnagrunninum
+
+        public void TengingVidGagnagrunn()
+        {
+            server = "10.200.10.24"; // ip tala serversins
+            database = "0701882449_hrutaspil"; // nafn databeisisins
+            uid = "0701882449"; // kennitala
+            password = "mypassword"; // lykilorð
+
+            tengistrengur = "server=" + server + ";userid=" + uid + ";password=" + password + ";database=" + database;
+            sqltenging = new MySqlConnection(tengistrengur);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                sqltenging.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        private bool Closeconnection()
+        {
+            try
+            {
+                sqltenging.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<string> LesautSQLToflu()
+        {
+            List<string> Faerslur = new List<string>();
+            string lina = null;
+            if (OpenConnection() == true) // ef næst er tengingu við gagnagrunn keyrist eftirfarandi kóði
+            {
+                fyrirspurn = "SELECT id, nafn, sort, thyngd, mjolk, ull, afkvaemi, einkunlaeris, frjosemi, gerd, einkunmalir FROM hrutar"; // Velur nafn og stokk úr gagnagrunninum
+                nySQLskipun = new MySqlCommand(fyrirspurn, sqltenging);
+
+                sqllesari = nySQLskipun.ExecuteReader();
+                while (sqllesari.Read())
+                {
+                    for (int i = 0; i < sqllesari.FieldCount; i++)
+                    {
+                        lina += (sqllesari.GetValue(i).ToString()) + ":";
+                    }
+                    Faerslur.Add(lina);
+                    lina = null;
+                }
+                Closeconnection();
+                return Faerslur;
+            }
+            return Faerslur;
+        }
+    }
+}
